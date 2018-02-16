9f6181897c8cef8ac3f57a04dc76afac32506fe6
vyshka <egorvyshka@gmail.com>
2018-02-16 22:05:59 +0300

    init commit

---
+++ script.js
@@ -0,0 +1,53 @@
+var btn = document.getElementById('sort');
+var array;
+btn.addEventListener("click", function(){
+    var input = document.getElementById("input").value;  
+    array = input.split(' ').map(function(item) {
+        return parseInt(item, 10);
+    });
+    array = quickSort(array, 0, array.length - 1);
+    var output = document.getElementById('output');
+    console.log(array);
+    var outputText = array.map(String);
+
+    output.value = outputText;
+});
+
+
+
+
+function quickSort(arr, left, right){
+    var len = arr.length, 
+    pivot,
+    partitionIndex;
+ 
+ 
+   if(left < right){
+     pivot = right;
+     partitionIndex = partition(arr, pivot, left, right);
+     
+    quickSort(arr, left, partitionIndex - 1);
+    quickSort(arr, partitionIndex + 1, right);
+   }
+   return arr;
+ }
+
+ function partition(arr, pivot, left, right){
+    var pivotValue = arr[pivot],
+        partitionIndex = left;
+ 
+    for(var i = left; i < right; i++){
+     if(arr[i] < pivotValue){
+       swap(arr, i, partitionIndex);
+       partitionIndex++;
+     }
+   }
+   swap(arr, right, partitionIndex);
+   return partitionIndex;
+ }
+
+ function swap(arr, i, j){
+    var temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+ }
